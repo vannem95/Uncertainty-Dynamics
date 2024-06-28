2577ded4255a50a4a6105139f292f794277cfd4c
vannem95 <va19e@fsu.edu>
2024-06-28 15:17:57 -0400

    wip - gaussian conditional probability update - testing pdf 2

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 from scipy.stats import multivariate_normal
 import numpy as np
-x = np.array([[1,2], [3,4]])
-multivariate_normal.pdf(x, mean=[0, 1], cov=[5, 2])
+import math
+
+x = 0.1
+x2 = multivariate_normal.pdf(x, mean=[0], cov=[1])
+
+print(x2)
+
+# test gaussian
+mu = 0
+sig = 1
+x3 = (1/(sig*(2*np.pi)**0.5))*math.exp(-0.5* ((x - mu)/(sig))**2 )
+print(x3)
